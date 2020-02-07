7df0e32947b0300f0477468e517be9bd07005cbe
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-07 15:25:04 +0800

    Use reusable hooks

---
+++ src/hooks/useRestaurants.js
@@ -0,0 +1,30 @@
+import {useEffect, useState} from 'react';
+import yelp from '../api/yelp';
+
+export default () => {
+  const [restaurants, setRestaurants] = useState([]);
+  const [error, setError] = useState('');
+
+  const searchRestaurants = async term => {
+    try {
+      const res = await yelp.get(
+        `/search?term=${term}&limit=50&location=Manila`,
+      );
+      setRestaurants(res.data.businesses);
+      setError('');
+    } catch (err) {
+      if (err.response.data.error.field === 'Authorization') {
+        setError('Validation Error');
+      } else {
+        setError(err.response.data.error.description);
+      }
+      setRestaurants([]);
+    }
+  };
+
+  useEffect(() => {
+    (async () => await searchRestaurants('Jollibee'))();
+  }, []);
+
+  return [restaurants, error, searchRestaurants];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,38 +1,19 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useRestaurant from '../hooks/useRestaurants';
 
 const styles = StyleSheet.create({});
 
 const SearchScreen = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [restaurants, setRestaurants] = useState([]);
-  const [error, setError] = useState('');
-
-  const searchRestaurants = async () => {
-    try {
-      const res = await yelp.get(
-        `/search?term=${searchTerm}&limit=50&location=Manila`,
-      );
-      setRestaurants(res.data.businesses);
-      setError('');
-    } catch (err) {
-      if (err.response.data.error.field === 'Authorization') {
-        setError('Validation Error');
-      } else {
-        setError(err.response.data.error.description);
-      }
-      setRestaurants([]);
-    }
-  };
-
+  const [restaurants, error, searchRestaurants] = useRestaurant();
   return (
     <View>
       <SearchBar
         searchTerm={searchTerm}
         setSearchTerm={setSearchTerm}
-        onTermSubmit={searchRestaurants}
+        onTermSubmit={async () => await searchRestaurants(searchTerm)}
       />
       {restaurants.length > 0 && (
         <Text>WE have found {restaurants.length} restaurants</Text>
